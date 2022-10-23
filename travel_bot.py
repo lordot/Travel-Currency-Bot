0f8f8abd0876b9cc9e6e20485589fc8bba6ba46a
Votoryncev Dmitry <w.dmkvo@gmail.com>
2022-10-23 20:23:01 +0300

    v 1.1

--- travel_bot.py
+++ travel_bot.py
@@ -4,11 +4,9 @@ import sys
 
 from telegram.ext import Updater, MessageHandler, Filters
 
-TOKEN = os.getenv('TOKEN')
-PORT = int(os.environ.get('PORT', '8443'))
-APP_NAME = os.getenv('APP_NAME')
-IDRDOL = float(os.getenv('IDRDOL'))
-IDRRUB = float(os.getenv('IDRRUB'))
+TOKEN = '5542961975:AAHHVziYdOyIU5giQmNEBGm4JYY0idksn1Y'
+IDRDOL = 14.975
+DOLRUB = 62.5
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -21,7 +19,7 @@ def exchange(update, context):
     chat = update.effective_chat
     value = float(update.message.text)
     dollars = round(float(value/IDRDOL), 1)
-    rubles = round(float(dollars*IDRRUB), 1)
+    rubles = round(float(dollars*DOLRUB), 1)
     message = f'{dollars}$\n{rubles}₽'
     context.bot.send_message(
         chat_id=chat.id,
@@ -33,12 +31,7 @@ def exchange(update, context):
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$'), exchange))
-    updater.start_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        url_path=TOKEN,
-        webhook_url=f"https://{APP_NAME}.herokuapp.com/{TOKEN}"
-    )
+    updater.start_polling(2)
     updater.idle()
 
 
